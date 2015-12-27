d7c83f7536127ffbaa71b937ba66fb29e9530b6b
doodad-js <sickle2@users.noreply.github.com>
2015-12-27 18:17:17 +0000

    - <Fix> Tools.Url and Tools.Path - <Fix> Tools.safeEval: Unauthorized global variables can be acceded - <Fix> NodeJs.Server.Http: Was resolving on error - <Fix> Tools.Files.openFile: Was resolving on error - <Fix> Doodad.Tools.Locale.setLocale: Was resolving on error - <Fix> Doodad.Tools.Locale.setLocale: IE doesn't understand "utf8", but it understands "utf-8". - <Fix> Doodad.Tools.readFile (client).: Was resolving on error - <Fix> Terminal: "exit" and "quit" commands was no longer working (regression) - <Fix> Terminal: Error when command is only spaces - <Fix> Client: Missing "var" - <Fix> Tools.Config.loadFile: Multiple fixes - <Fix> "Set" polyfill: Constructor on array was not creating a Set with unique items - <New> NodeJs.Server.Http.StaticPage: Multi-paths - <New> Server.Http.Page: "$prepare" - <New> IO.Stream: Implement "pipe" and "unpipe". - <New> Doodad.Tools.Locale: New settings option "localePath" - <New> Promise.nodeify: Replaces previous "finally" polyfill - <New> Modules.load and Modules.locate - <Chg> Tools.Config: Client expects URLs, Server expects system paths. - <Chg> Loader: Rewritten with Promises - <Chg> IO.PipeStream and IO.TextPipeStream: Replaced by "IO.Stream.pipe". - <Chg> Project splited into separated packages - <Chg> Module skeleton: When server side, exports the function "add" and no longer use "global" to store modules in DD_MODULES. - <Chg> NodeJs.Server.Http.StaticPage: Rename "templateFolder" to "folderTemplate". - <Chg> Server.Http.JsonRpc.Page: "GET" now returns a JSON-RPC response like "POST". - <Chg> Bootstrap has its own options, moved from Types. - <Chg> Bootstrap no longer loads non-bootstrap namespaces. They must be loaded separately using "Namespaces.loadNamespaces". - <Chg> Namespaces: Rewritten with Promises - <Chg> Namespaces.loadNamespaces: Add new "callback" parameter, at position 0. - <Chg> Promise.finalize Polyfill: Works like Bluebird's finalize function. - <Chg> NodeJs.getPackageInfo removed. - <Chg> NodeJs.requireModule: Replaced by Modules.load - <Chg> NodeJs.resolveClientModule: Replaces by Modules.locate

--- src/common/Tools_Mime.js
+++ src/common/Tools_Mime.js
@@ -1,5 +1,5 @@
 //! REPLACE_BY("// Copyright 2015 Claude Petit, licensed under Apache License version 2.0\n")
-// dOOdad - Class library for Javascript (BETA) with some extras (ALPHA)
+// dOOdad - Object-oriented programming framework with some extras
 // File: Tools_Mime.js - Mime Tools
 // Project home: https://sourceforge.net/projects/doodad-js/
 // Trunk: svn checkout svn://svn.code.sf.net/p/doodad-js/code/trunk doodad-js-code
@@ -26,93 +26,107 @@
 (function() {
 	var global = this;
 
-	global.DD_MODULES = (global.DD_MODULES || {});
-	global.DD_MODULES['Doodad.Tools.Mime'] = {
-		type: null,
-		version: '0d',
-		namespaces: null,
-		dependencies: ['Doodad.Tools'],
-		
-		create: function create(root, /*optional*/_options) {
-			"use strict";
-			
-			var doodad = root.Doodad,
-				types = doodad.Types,
-				tools = doodad.Tools,
-				namespaces = doodad.Namespaces,
-				config = tools.Config,
-				mime = tools.Mime;
-			
-			var __Internal__ = {
-				mimeExtensions: null,
-				mimeTypes: null,
-			};
-
-			mime.getTypes = function(fileName, /*optional*/defaultType) {
-				if (types.isNothing(fileName)) {
-					return [];
-				};
-				var pos = fileName.lastIndexOf('.');
-				if (pos >= 0) {
-					fileName = fileName.slice(pos + 1);
-				};
-				if (defaultType && !types.isArray(defaultType)) {
-					defaultType = [defaultType];
-				};
-				return types.get(__Internal__.mimeExtensions, fileName.toLowerCase(), defaultType || ['application/octet-stream']);
-			};
-			
-			mime.getExtensions = function(mimeType, /*optional*/defaultExtension) {
-				if (types.isNothing(mimeType)) {
-					return [];
-				};
-				if (defaultExtension && !types.isArray(defaultExtension)) {
-					defaultExtension = [defaultExtension];
-				};
-				return types.get(__Internal__.mimeTypes, mimeType.toLowerCase(), defaultExtension || ['']);
-			};
-			
-			mime.getSupportedTypes = function() {
-				return types.keys(__Internal__.mimeTypes);
-			};
-			
-			mime.getKnownExtensions = function() {
-				return types.keys(__Internal__.mimeExtensions);
-			};
-			
-			
-			__Internal__.parseMimeExtensions = function parseMimeExtensions(data) {
-				if (!types.isError(data)) {
-					__Internal__.mimeExtensions = data.mimeExtensions;
-					var mimeTypes = __Internal__.mimeTypes = {};
-					tools.forEach(data.mimeExtensions, function(mTypes, extension) {
-						tools.forEach(mTypes, function(mType) {
-							if (types.hasKey(mimeTypes, mType)) {
-								mimeTypes[mType].push(extension);
-							} else {
-								mimeTypes[mType] = [extension];
-							};
-						});
-					}, {});
-				};
-			};
-			
-			mime.loadTypes = function loadTypes(/*optional*/callback) {
-				// Remote file test
-				//var configPath = null;
-				//config.loadFile('http://localhost/doodadjs/res/mimeExtensions.json', 
-					
-				var configPath = tools.getCurrentScript((global.document?document.currentScript:module.filename)||(function(){try{throw new Error("");}catch(ex){return ex;}})());
-				var path = tools.options.hooks.pathParser('./res/mimeExtensions.json');
-				return config.loadFile(path, {async: true, watch: true, configPath: configPath, encoding: 'utf8'}, [__Internal__.parseMimeExtensions, callback]);
-			};
-
-
-			return function init(/*optional*/options) {
-				mime.loadTypes()
-					['finally'](new namespaces.ReadyCallback());
-			};
-			
-		},
+	var exports = {};
+	if (global.process) {
+		module.exports = exports;
 	};
+	
+	exports.add = function add(DD_MODULES) {
+		DD_MODULES = (DD_MODULES || {});
+		DD_MODULES['Doodad.Tools.Mime'] = {
+			type: null,
+			version: '0b',
+			namespaces: null,
+			dependencies: ['Doodad.Tools'],
+			
+			create: function create(root, /*optional*/_options) {
+				"use strict";
+				
+				var doodad = root.Doodad,
+					types = doodad.Types,
+					tools = doodad.Tools,
+					namespaces = doodad.Namespaces,
+					config = tools.Config,
+					mime = tools.Mime;
+				
+				var __Internal__ = {
+					mimeExtensions: null,
+					mimeTypes: null,
+				};
+
+				mime.getTypes = function(fileName, /*optional*/defaultType) {
+					if (types.isNothing(fileName)) {
+						return [];
+					};
+					var pos = fileName.lastIndexOf('.');
+					if (pos >= 0) {
+						fileName = fileName.slice(pos + 1);
+					};
+					if (defaultType && !types.isArray(defaultType)) {
+						defaultType = [defaultType];
+					};
+					return types.get(__Internal__.mimeExtensions, fileName.toLowerCase(), defaultType || ['application/octet-stream']);
+				};
+				
+				mime.getExtensions = function(mimeType, /*optional*/defaultExtension) {
+					if (types.isNothing(mimeType)) {
+						return [];
+					};
+					if (defaultExtension && !types.isArray(defaultExtension)) {
+						defaultExtension = [defaultExtension];
+					};
+					return types.get(__Internal__.mimeTypes, mimeType.toLowerCase(), defaultExtension || ['']);
+				};
+				
+				mime.getSupportedTypes = function() {
+					return types.keys(__Internal__.mimeTypes);
+				};
+				
+				mime.getKnownExtensions = function() {
+					return types.keys(__Internal__.mimeExtensions);
+				};
+				
+				
+				__Internal__.parseMimeExtensions = function parseMimeExtensions(err, data) {
+		//console.log(data);
+					if (!err) {
+						__Internal__.mimeExtensions = data.mimeExtensions;
+						var mimeTypes = __Internal__.mimeTypes = {};
+						tools.forEach(data.mimeExtensions, function(mTypes, extension) {
+							tools.forEach(mTypes, function(mType) {
+								if (types.hasKey(mimeTypes, mType)) {
+									mimeTypes[mType].push(extension);
+								} else {
+									mimeTypes[mType] = [extension];
+								};
+							});
+						}, {});
+					};
+				};
+				
+				mime.loadTypes = function loadTypes(/*optional*/callback) {
+					// Remote file test
+					//var configPath = null;
+					//config.loadFile('http://localhost/doodadjs/res/mimeExtensions.json', 
+						
+					var configPath = tools.getCurrentScript((global.document?document.currentScript:module.filename)||(function(){try{throw new Error("");}catch(ex){return ex;}})());
+					var path = tools.options.hooks.pathParser('./res/mimeExtensions.json');
+					return config.loadFile(path, {async: true, watch: true, configPath: configPath, encoding: 'utf8'}, [__Internal__.parseMimeExtensions, callback]);
+				};
+
+
+				return function init(/*optional*/options) {
+					return mime.loadTypes();
+				};
+				
+			},
+		};
+		
+		return DD_MODULES;
+	};
+	
+	if (!global.process) {
+		// <PRB> export/import are not yet supported in browsers
+		global.DD_MODULES = exports.add(global.DD_MODULES);
+	};	
 })();
